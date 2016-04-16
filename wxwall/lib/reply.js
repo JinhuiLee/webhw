28e92934e015618eb5f3626d1fa50dce06d86baf
Jinhui Li <jinhui.li@pku.edu.cn>
2016-04-16 10:04:55 -0500

    when user send a msg , send it back as an echo

--- wxwall/lib/reply.js
+++ wxwall/lib/reply.js
@@ -23,6 +23,76 @@ function replyText(msg, replyText){
   });
 }
 
+function replyPic(msg, replyPic){
+  console.log("HERE4************");
+  //将要返回的消息通过一个简单的tmpl模板（npm install tmpl）返回微信
+  var tmpl = require('tmpl');
+  var replyTmpl = '<xml>' +
+    '<ToUserName><![CDATA[{toUser}]]></ToUserName>' +
+    '<FromUserName><![CDATA[{fromUser}]]></FromUserName>' +
+    '<CreateTime><![CDATA[{time}]]></CreateTime>' +
+    "<MsgType><![CDATA[image]]></MsgType>"+
+    "<Image>"+
+    "<MediaId><![CDATA[{imageid}]]></MediaId>"+
+    "</Image>"+
+    "</xml>";
+  console.log("ready to send back info");
+  return tmpl(replyTmpl, {
+    toUser: msg.xml.FromUserName[0],
+    fromUser: msg.xml.ToUserName[0],
+    time: Date.now(),
+    imageid: "7-XnmilOrKUF45ua0IgZ4bTij09CJ4sOeJYJJnFw6tH-Lw4ICOT6PS7lGCm9795B"
+  });
+}
+
+function replyVoice(msg, replyPic){
+  console.log("HERE4************");
+  //将要返回的消息通过一个简单的tmpl模板（npm install tmpl）返回微信
+  var tmpl = require('tmpl');
+  var replyTmpl = '<xml>' +
+    '<ToUserName><![CDATA[{toUser}]]></ToUserName>' +
+    '<FromUserName><![CDATA[{fromUser}]]></FromUserName>' +
+    '<CreateTime><![CDATA[{time}]]></CreateTime>' +
+    "<MsgType><![CDATA[voice]]></MsgType>"+
+    "<Voice>"+
+    "<MediaId><![CDATA[{voiceid}]]></MediaId>"+
+    "</Voice>"+
+    "</xml>";
+  console.log("ready to send back info");
+  return tmpl(replyTmpl, {
+    toUser: msg.xml.FromUserName[0],
+    fromUser: msg.xml.ToUserName[0],
+    time: Date.now(),
+    voiceid: msg.xml.MediaId
+  });
+}
+
+function replyVideo(msg, replyPic){
+  console.log("HERE4************");
+  //将要返回的消息通过一个简单的tmpl模板（npm install tmpl）返回微信
+  var tmpl = require('tmpl');
+  var replyTmpl = '<xml>' +
+    '<ToUserName><![CDATA[{toUser}]]></ToUserName>' +
+    '<FromUserName><![CDATA[{fromUser}]]></FromUserName>' +
+    '<CreateTime><![CDATA[{time}]]></CreateTime>' +
+    "<MsgType><![CDATA[shortvideo]]></MsgType>"+
+    "<MediaId><![CDATA[{videoid}]]></MediaId>"+
+    "<ThumbMediaId><![CDATA[{tmbid}]]></ThumbMediaId>"+
+    "</xml>";
+  console.log("ready to send back info");
+  return tmpl(replyTmpl, {
+    toUser: msg.xml.FromUserName[0],
+    fromUser: msg.xml.ToUserName[0],
+    time: Date.now(),
+    videoid: msg.xml.MediaId,
+    tmbid: msg.xml.ThumbMediaId
+  });
+}
+
+
 module.exports = {
-  replyText: replyText
+  replyText: replyText,
+  replyPic: replyPic,
+  replyVideo: replyVideo,
+  replyVoice: replyVoice
 };
--- wxwall/verify.js
+++ wxwall/verify.js
@@ -6,6 +6,10 @@ var TOKEN="jinhui"
 
 var getUserInfo = require('./lib/user').getUserInfo;
 var replyText = require('./lib/reply').replyText; 
+var replyPic = require('./lib/reply').replyPic;
+var replyVoice = require('./lib/reply').replyVoice;
+var replyVideo = require('./lib/reply').replyVideo;
+
 
 var wss = require('./lib/ws.js').wss;
 var msgBuffer= require('./lib/ws.js').buffer;
@@ -40,22 +44,55 @@ var server=http.createServer(function (request,response) {
     request.addListener('end',function(){
       var parseString = require('xml2js').parseString;
       parseString(postdata,function (err,result){
+        console.log(postdata);
         if(result.xml.MsgType[0] === 'text'){
           getUserInfo(result.xml.FromUserName[0])
             .then(function(userInfo){
-              //获得用户信息，合并到消息中
-	      console.log("HERE**************");
               result.user = userInfo;
-              console.log("HERE1*************");
               //将消息通过websocket广播
-              console.log("HERE2*************");
               var res = replyText(result, '消息推送成功！<a href="http://123.206.79.164/index.html">查看微信墙</a>');
-              console.log("HERE3**********");
               response.end(res);
 	      wss.broadcast(result);
 	      msgBuffer.push(result);
             })
         }
+	else if(result.xml.MsgType[0] === 'image'){
+          getUserInfo(result.xml.FromUserName[0])
+            .then(function(userInfo){
+              result.user = userInfo;
+              //将消息通过websocket广播
+              var res = replyPic(result, '消息推送成功！<a href="http://123.206.79.164/index.html">查看>微信墙</a>');
+              response.end(res);
+              wss.broadcast(result);
+              msgBuffer.push(result);
+            })
+        }
+	else if(result.xml.MsgType[0] === 'voice'){
+          getUserInfo(result.xml.FromUserName[0])
+            .then(function(userInfo){
+              result.user = userInfo;
+              //将消息通过websocket广播
+              console.log("voice herer");
+              var res = replyVoice(result, '消息推送成功！<a href="http://123.206.79.164/index.html">查看>微信墙</a>');
+              console.log("voice: "+res);
+              response.end(res);
+              wss.broadcast(result);
+              msgBuffer.push(result);
+            })
+        }
+	else if(result.xml.MsgType[0] === 'shortvideo'){
+          getUserInfo(result.xml.FromUserName[0])
+            .then(function(userInfo){
+              result.user = userInfo;
+              //将消息通过websocket广播
+              var res = replyVideo(result, '消息推送成功！<a href="http://123.206.79.164/index.html">查看>微信墙</a>');
+              console.log("video: "+res);
+              response.end(res);
+              wss.broadcast(result);
+              msgBuffer.push(result);
+            })
+        }
+
       });
     });
   }
