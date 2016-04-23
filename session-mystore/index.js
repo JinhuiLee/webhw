e442b326da1c67d928ee5b1328034615eb9403c4
Jinhui Li <jinhui.li@pku.edu.cn>
2016-04-23 12:51:34 +0800

    implement a basic session-store middleware on fs

---
+++ session-mystore/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var parseurl = require('parseurl');
+var myStore = require('./session-myfs.js')(session);
+
+var app = express();
+
+app.use(session({
+	secret: 'keyboard cet',
+	resave: 'false',
+	saveUninitialized: true,
+	store: new myStore()
+}));
+
+app.use(function(req,res,next){
+  var views = req.session.views;
+  if (!views){
+    views = req.session.views = {}
+  }
+
+  var pathname = parseurl(req).pathname;
+  views[pathname] = (views[pathname]||0)+1;
+  next();
+});
+
+app.get('/',function(req,res,next){
+	res.send("You have viewed this page " + req.session.views["/"] + " times");
+});
+
+
+app.use(cookieParser());
+
+
+app.get('/read',function(req,res,next){
+	res.json(req.cookies);
+});
+
+app.get('/write',function(req,res,next){
+	res.cookie('my_cookie','hello');
+	res.json(req.cookies);
+});
+
+app.listen(3000);
+console.log("Server running at port : 3000");
---
+++ session-mystore/session-myfs.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+var path = require('path');
+var sessionDir="./session";
+
+
+
+module.exports=function(session) {
+
+  var Store = session.Store;
+
+
+  console.log("myfs");
+  function MyFStore(options) {
+    var self = this;
+
+    options = options || {};
+    Store.call(self, options);
+  }
+
+  function dirExists(dir)
+  {
+    var exists = fs.existsSync(path.normalize(dir));
+    if (!exists) return false;
+
+    var stats = fs.statSync(path.normalize(dir));
+    if ( !stats.isDirectory() ) return false;
+    return true;
+  }
+
+  MyFStore.prototype.__proto__ = Store.prototype;
+  function sid2Path(sid)
+  {
+    var filePath = path.normalize(path.join(sessionDir,sid));
+    console.log(filePath);
+    return filePath;
+  }
+
+
+  MyFStore.prototype.get = function(sid,callback)
+  {
+    console.log("get");
+    var filePath = sid2Path(sid);
+    var sjson = fs.readFileSync(filePath,'utf8');
+    var session = JSON.parse(sjson);
+    callback(null,session);
+  }
+
+
+  MyFStore.prototype.set = function(sid,session,callback)
+  {
+    console.log("set");
+    if (!dirExists(sessionDir))
+      fs.mkdirSync(sessionDir);
+
+
+    var filePath = sid2Path(sid);
+    var sjson = JSON.stringify(session);
+    fs.writeFileSync(filePath,sjson,'utf8');
+    callback(null);
+  }
+
+
+  MyFStore.prototype.destroy = function(sid,callback)
+  {
+    var filePath = sid2Path(sid);
+    fs.unlinkSync(filePath);
+    callback(null);
+  }
+  return MyFStore;
+}
